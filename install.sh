0d67f1ce46577bcb5beb9e667b936d8e168a60e7
Harsh Gandhi <harshnar@buffalo.edu>
2020-03-12 12:25:31 -0700

    mkdir added

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 # Installing AWS CLI
-cd /home/ec2-user/
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/ec2-user/awscliv2.zip"
+mkdir a1
 unzip /home/ec2-user/awscliv2.zip
+mkdir a2
 sudo /home/ec2-user/aws/install
-
+mkdir a3
 # Configuring AWS CLI
-# aws configure set aws_access_key_id $1
-# aws configure set aws_secret_access_key $2
-# aws configure set region $3
-# aws configure set output json
+aws configure set aws_access_key_id $1
+mkdir a4
+aws configure set aws_secret_access_key $2
+mkdir a5
+aws configure set region $3
+mkdir a6
+aws configure set output json
+mkdir a7
 
 # Install AWSIoTSDK
 sudo pip install awsiotsdk
+mkdir a8
