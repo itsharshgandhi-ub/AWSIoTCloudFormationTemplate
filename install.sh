2b177fa5accd0978ea3ed8fc67543ab744899ebb
Harsh Gandhi <harshnar@buffalo.edu>
2020-03-13 09:51:57 -0700

    mkdir added

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 # Installing AWS CLI
+cd /home/ec2-user
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/ec2-user/awscliv2.zip"
 unzip /home/ec2-user/awscliv2.zip
 sudo /home/ec2-user/aws/install
 sudo ./aws/install
+
 # Configuring AWS CLI
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
